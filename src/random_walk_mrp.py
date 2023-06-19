69046e61ab578473087d34bb7fe2d25c027cd0af
tfe <Takfes@users.noreply.github.com>
2023-06-20 01:07:36 +0300

    add td0 - needs fix

--- src/random_walk_mrp.py
+++ src/random_walk_mrp.py
@@ -5,14 +5,21 @@ import matplotlib.pyplot as plt
 
 
 class RandomWalkMRP:
-    def __init__(self, transition_matrix, rewards_vector):
+    def __init__(self):
         # Define the transition matrix
-        self.transition_matrix = transition_matrix
-        assert (
-            self.transition_matrix.shape[0] == self.transition_matrix.shape[0]
-        ), f"Transition matrix must be square, shape provided {self.transition_matrix.shape=}"
+        self.transition_matrix = np.array(
+            [
+                [1, 0, 0, 0, 0, 0, 0],
+                [0.5, 0, 0.5, 0, 0, 0, 0],
+                [0, 0.5, 0, 0.5, 0, 0, 0],
+                [0, 0, 0.5, 0, 0.5, 0, 0],
+                [0, 0, 0, 0.5, 0, 0.5, 0],
+                [0, 0, 0, 0, 0.5, 0, 0.5],
+                [0, 0, 0, 0, 0, 0, 1],
+            ]
+        )
         # Define the reward vector
-        self.rewards_vector = rewards_vector
+        self.rewards_vector = np.array([0, 0, 0, 0, 0, 0, 1])
         self.terminal_states = [0, self.transition_matrix.shape[0] - 1]
 
     def transition(self, state):
@@ -107,34 +114,64 @@ def mc_constant_alpha(episodes, alpha=0.1, gamma=1.0):
     return V
 
 
+def td_0(episodes, alpha=0.1, gamma=1.0):
+    """
+    TD(0) Value Estimation.
+
+    :param episodes: A dictionary of dicitonaries.
+    like so:
+    {
+        0: {
+            'states': [4, 3, 2, 3, 2, 3, 4, 3, 4, 3, 2, 1, 0],
+            'rewards': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+        1: {
+            'states': [2, 1, 2, 3, 2, 3, 4, 5, 4, 5, 6],
+            'rewards': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+            }
+    }
+    :param alpha: The step-size parameter.
+    :param gamma: The discount factor.
+    """
+    # Initialize the value estimate
+    V = defaultdict(float)
+
+    # For each episode
+    for _, v in episodes.items():
+        states, rewards = v["states"], v["rewards"]
+        # Iterate until the last state (not including the terminal state)
+        for i in range(len(states) - 1):
+            # Compute the TD target
+            TD_target = rewards[i] + gamma * V[states[i + 1]]
+            # Update the value estimate
+            V[states[i]] += alpha * (TD_target - V[states[i]])
+
+    return V
+
+
 if __name__ == "__main__":
+    # ==============================================
+    # Settings
+    # ==============================================
     # Define number of MRP episodes to sample
-    N_EPISODES = 1000
-    MC_CONSTANT_ALPHA = 0.005
-
-    # Define the transition matrix
-    transition_matrix = np.array(
-        [
-            [1, 0, 0, 0, 0, 0, 0],
-            [0.5, 0, 0.5, 0, 0, 0, 0],
-            [0, 0.5, 0, 0.5, 0, 0, 0],
-            [0, 0, 0.5, 0, 0.5, 0, 0],
-            [0, 0, 0, 0.5, 0, 0.5, 0],
-            [0, 0, 0, 0, 0.5, 0, 0.5],
-            [0, 0, 0, 0, 0, 0, 1],
-        ]
-    )
-
-    # Define the reward vector
-    rewards = np.array([0, 0, 0, 0, 0, 0, 1])
-
+    N_EPISODES = 1_000
+    ALPHA = 0.01
     # Create an MRP
-    mrp = RandomWalkMRP(transition_matrix, rewards)
-
+    mrp = RandomWalkMRP()
+    shape = mrp.transition_matrix.shape[0]
     # Sample episodes from MRP
     episodes = mrp.sample_episodes(N_EPISODES, 3)
 
+    # ==============================================
+    # Compute the expected value estimates
+    # ==============================================
+    df_exp = pd.DataFrame(
+        [(1 / (shape - 1)) * x for x in range(shape)],
+        columns=["expected"],
+    )
+
+    # ==============================================
     # Compute the value estimates based on first visit MC
+    # ==============================================
     Vfv = mc_first_visit(episodes)
     df_mcfv = (
         pd.DataFrame()
@@ -142,27 +179,34 @@ if __name__ == "__main__":
         .sort_index()
         .rename(columns={0: "mc_first_visit"})
     )
+    df = df_exp.join(df_mcfv)
 
+    # ==============================================
     # Compute the value estimates based on constant value MC
-    Vca = mc_constant_alpha(episodes, alpha=MC_CONSTANT_ALPHA)
+    # ==============================================
+    Vca = mc_constant_alpha(episodes, alpha=ALPHA)
     df_mcca = (
         pd.DataFrame()
         .from_dict(Vca, orient="index")
         .sort_index()
         .rename(columns={0: "mc_constant_alpha"})
     )
+    df = df.join(df_mcca)
 
-    # Compute the expected value estimates
-    df_exp = pd.DataFrame(
-        [
-            (1 / (transition_matrix.shape[0] - 1)) * x
-            for x in range(transition_matrix.shape[0])
-        ],
-        columns=["expected"],
-    )
+    # ==============================================
+    # Compute the value estimates based on TD(0)
+    # ==============================================
+    # Vtd0 = td_0(episodes, alpha=ALPHA)
+    # df_vtd0 = (
+    #     pd.DataFrame()
+    #     .from_dict(Vtd0, orient="index")
+    #     .sort_index()
+    #     .rename(columns={0: "td_0"})
+    # )
+    # df = df.join(df_vtd0)
 
-    df = df_exp.join(df_mcfv).join(df_mcca)
-
+    # ==============================================
     # Plot the DataFrame with lines and dots
+    # ==============================================
     df.plot(marker="o", linestyle="-", grid=True)
     plt.show()
